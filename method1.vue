56333bd1a5a06f5945d9b0fe2207c0e589bca0f9
sukanya-500apps <102227233+sukanya-500apps@users.noreply.github.com>
2022-03-30 19:31:14 +0530

    Add files via upload

---
+++ method1.vue
@@ -0,0 +1,47 @@
+<template>
+
+<div>
+
+<h1>ES6 methods </h1>
+
+<h3>{{name}}</h3>
+
+<button @click="fun()">change_name</button>
+
+</div>
+
+</template>
+
+<script>
+
+
+export default {
+
+name:"SukanyA",
+
+data(){
+
+return{
+
+name:"mantra",
+
+}
+
+},
+
+
+methods:{
+
+fun(){
+
+this.name="technologies";
+
+return this.name ;
+
+}
+
+}}
+
+
+</script>
+
