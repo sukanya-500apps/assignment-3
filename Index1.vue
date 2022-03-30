feb9567bf409c58d98571b0aad2dc18c3ad950db
sukanya-500apps <102227233+sukanya-500apps@users.noreply.github.com>
2022-03-30 17:01:10 +0530

    Add files via upload

---
+++ Index1.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="hello">
+        <p>users = {{ users }}</p>
+	<p>Element Index = {{elmIndex}}</p>
+
+	<button @click="myFunction()">Click Me</button>
+    </div>
+</template>
+<script>
+export default {
+    name:'IndexFirst',
+data(){
+    return{
+users:[ { id: 20, name: 'Captain Piett' },
+{ id: 24, name: 'General Veers' }, 
+{ id: 56, name:'Admiral Ozzel' },
+ { id: 88, name: 'Commander Jerjerrod' } ],
+elmIndex:''
+}
+},
+methods:
+{
+    myFunction() {
+    this.elmIndex=this.users.findIndex((data)=>{
+        return data.id == '88'
+    })
+    console.log(this.elmIndex);
+},
+}
+}
+
+
+</script>
