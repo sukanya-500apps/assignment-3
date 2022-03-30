087404c7c459e734bad03bd511bd6b9e227f5094
sukanya-500apps <102227233+sukanya-500apps@users.noreply.github.com>
2022-03-30 17:53:19 +0530

    Add files via upload

---
+++ question1.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <table align="centre">
+            <thead>
+                <th>Name</th>
+                <th>Index</th>
+                </thead>
+                <tbody>
+                    <tr v-for="user in users" :key="user.id">
+                        <td>{{user.name}}</td>
+                        <td>{{user.Index}}</td>
+                    </tr>
+                </tbody>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    name:"QuestioN1",
+    data(){
+        return{
+users:[{"name":"AgileCRM" , "Index" : "1"},
+
+{"name":"Mantra Technologies" , "Index" : "2"},
+
+{"name":"500apps Python" , "Index" : "3"},
+
+{"name":"500apps Infinity" , "Index" : "4"}]
+
+}
+
+},
+
+}
+</script>
