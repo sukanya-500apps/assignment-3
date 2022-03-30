4998bfd02ebfe669da57ef7e89985078091aa5f4
sukanya-500apps <102227233+sukanya-500apps@users.noreply.github.com>
2022-03-30 19:32:30 +0530

    Add files via upload

---
+++ metod2.vue
@@ -0,0 +1,94 @@
+<template>
+
+<div>
+
+<h1>ES6 Filter method</h1>
+
+<h3>{{students}}</h3>
+
+<button @click="fun()">click</button>
+
+<button @click="f1()">for Each</button>
+
+<button @click="f2()">map</button>
+
+</div>
+
+</template>
+
+<script>
+
+
+export default {
+
+name:"MethoD2",
+
+data(){
+
+return{
+
+students :[
+
+{ name: 'Sukanya', grade: 96 },
+
+{ name: 'Naveen', grade: 84 },
+
+{ name: 'Chaitrika', grade: 80 },
+
+{ name: 'Navya', grade: 95 },
+
+{ name: 'Sri', grade: 90 },
+
+],
+
+array :[4,7,2,9,6,3]
+
+}
+
+},
+
+
+
+methods:{
+
+fun(){
+
+var studentGrades = this.students.filter(student => {return student.grade > 90});
+
+console.log(studentGrades);
+
+},
+
+f1(){
+
+this.array.forEach((arrayItems,index) => {
+
+console.log(arrayItems,index)
+
+})
+
+},
+
+f2(){
+
+this.array.map((arrayItems,index) => {
+
+if (index==2)return 0
+
+console.log(arrayItems,index)
+
+})
+
+}
+
+
+}
+
+}
+
+
+</script>
+
+
+
+
